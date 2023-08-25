2482e39ff306b8e937a2bcc4f4cd0c6a86ecbdb7
polashchandrae <polashchandra210@gmail.comm>
2023-08-26 03:14:47 +0600

    my bomi entu osshota

---
+++ area.js
@@ -0,0 +1,9 @@
+function clickhandle(){
+    const inputflid=document.getElementById('inputText');
+    const inputInnertext=inputflid.value;
+    const inputmainValue=parseFloat(inputInnertext)
+
+    const heightinput=document.getElementById('');
+    const heightinnertext=heightinput.value
+    const heightmainvalue=parseFloat(heightinnertext)
+}
