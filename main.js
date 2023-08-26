095588c4b28cdb74604b0752fff8c0cf18b6a17d
polashchandrae <polashchandra210@gmail.comm>
2023-08-27 01:20:41 +0600

    my geomatry website

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function clickhandle(){
     const height=inputflid('heighttext')
     const widthheigt=0.5*witdth*height;
     tootalficed('vh-ariya',widthheigt)
+    addToCalculationEntry('trangle', widthheigt)
 
 }
 //Rectangle
@@ -24,6 +25,7 @@ function Rectangle(){
     const RectangleInput2=inputflid('inputwI2')
     const total=RectangleInput*RectangleInput2;
     tootalficed('wi-ariya1',total)
+    addToCalculationEntry('rectangle', total)
 }
 //paraparallelogram
 function paralelogram(){
@@ -31,6 +33,7 @@ function paralelogram(){
     const perainput2=inputflid('inputpara2');
     const total= parainput *perainput2;
     tootalficed('wi-ariya2',total)
+    addToCalculationEntry('paralelogram', total)
 }
 //Rhombus
 function Rhombus(){
@@ -38,6 +41,7 @@ function Rhombus(){
     const Rhombus2=inputflid('rhombusinput2')
     const total=0.5*Rhombus*Rhombus2;
     tootalficed('rhombus-ariya',total)
+    addToCalculationEntry('rhombus', total)
 }
 //pentagon
  function pentagon(){
@@ -45,6 +49,8 @@ function Rhombus(){
     const pentagoninput2=inputflid('pentagon2')
     const total = 0.5*penteninput*pentagoninput2;
     tootalficed('pentagon-ariya',total)
+    addToCalculationEntry('prentent', total)
+    
  }
  //ellipse
  function ellipse(){
@@ -53,3 +59,16 @@ function Rhombus(){
     const total=3.14*ellipseInput*ellipseInput2
     tootalficed('ellipse-ariya',total)
  }
+// main ariya side
+function addToCalculationEntry(areaType, area){
+    console.log(areaType+ ' '  + area)
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
+
+    calculationEntry.appendChild(p);
+}
